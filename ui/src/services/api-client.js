c0f7e318665a0c15a7a6223c7a86fb58aed1cf25
salamonm <salamonm@ryanair.com>
2020-01-24 16:54:37 +0100

    #7: lslint added

--- ui/src/services/api-client.js
+++ ui/src/services/api-client.js
@@ -1,33 +1,31 @@
-import apisauce from 'apisauce'
-
-import AppConfig from '../config/app-config'
+import apisauce from 'apisauce';
+import AppConfig from '../config/app-config';
 
 const create = (baseURL = AppConfig.API_URL) => {
-
   const api = apisauce.create({
     baseURL,
     headers: {
-      'Cache-Control': 'no-cache'
+      'Cache-Control': 'no-cache',
     },
-    timeout: AppConfig.API_CALL_TIMEOUT_MILLISECONDS
-  })
+    timeout: AppConfig.API_CALL_TIMEOUT_MILLISECONDS,
+  });
 
   const getElevators = (params) => api.get('/list', params);
   const moveElevator = (payload) => api.put('/move', payload);
   const requestElevator = (payload) => api.put('/request', payload);
   const releaseElevator = (payload) => api.put('/release', payload);
+  const naviMonitor = (response) => console.log('api call tracker: ', response);
 
-  const naviMonitor = (response) => console.log('api call tracker: ', response)
-  api.addMonitor(naviMonitor)
+  api.addMonitor(naviMonitor);
 
   return {
     getElevators,
     moveElevator,
     requestElevator,
     releaseElevator,
-  }
-}
+  };
+};
 
 export default {
-  create
-}
+  create,
+};
