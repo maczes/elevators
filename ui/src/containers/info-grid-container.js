73712fd39213ae67dc0f1da1e7c092dbe2a6d929
masala <maczes@gmail.com>
2020-01-25 16:45:35 +0100

    #2: info grid modified container

--- ui/src/containers/info-grid-container.js
+++ ui/src/containers/info-grid-container.js
@@ -2,26 +2,22 @@ import { connect } from 'react-redux';
 import InfoGrid from '../components/info-grid';
 
 import {
-    onPublishActivityReportAction,
+  onPublishActivityReportAction,
 } from '../actions/info-grid-action';
 
 const mapStateToProps = (state) => {
-
-    let newState = {
-        noticeBoard: state.onPublishActivityReportReducer.reports,
-    };
-    console.info(`info-grid reducer state  : ${JSON.stringify(newState)}`);
-    return newState;
+  const newState = {
+    noticeBoard: state.onPublishActivityReportReducer.reports,
+  };
+  console.info(`info-grid reducer state  : ${JSON.stringify(newState)}`);
+  return newState;
 };
 
-const mapDispatchToProps = (dispatch) => {
-
-    return {
-        onPublishActivityReport: (report) => {
-            dispatch(onPublishActivityReportAction(report));
-        },
-    };
-}
+const mapDispatchToProps = (dispatch) => ({
+  onPublishActivityReport: (report) => {
+    dispatch(onPublishActivityReportAction(report));
+  },
+});
 
 const InfoGridContainer = connect(mapStateToProps, mapDispatchToProps)(InfoGrid);
 
